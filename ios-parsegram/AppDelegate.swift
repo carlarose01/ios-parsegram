d415f5428b1257367fdd200b93c789f8e8ef14ab
Carla Rose <57645881+carlarose01@users.noreply.github.com>
2021-03-13 20:37:01 -0600

    added sign up and sign in feature

--- ios-parsegram/AppDelegate.swift
+++ ios-parsegram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "PELPFXscMGkRZax81uArpaH6Uw0hiif4NorMdcoA" // <- UPDATE
+                    $0.clientKey = "3BOja6BZaYrpSZh6NhpWAkrBTxshrR8cAzIoqsgz" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
